f842de238d0fd4242ffe4ac8905e616c133eb3ed
linxiaoye <512895265@qq.com>
2017-03-19 00:30:03 +0800

    add fill_n algorithm

--- TinySTL/Algorithm.h
+++ TinySTL/Algorithm.h
@@ -1,12 +1,46 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
+/*
+*   Author  @linxiaoye
+*/
+
+#include <cstring>
+#include <utility>
+#include "Allocator.h"
+// #include "Functional.h"
+#include "Iterator.h"
+#include "TypeTraits.h"
+// #include "Utility.h"
+
+
 namespace TinySTL 
 {
 	
+	/********************[ fill_n ]***********************************/
+	/********************[Algorithm Time Complexity: O(n)]************/
 	
-	
-	
+	template<class OutputIterator, class Size, class T>
+	OutputIterator fill_n(OutputIterator first, Size n, const T& value)
+	{
+		for ( ; n > 0; --n, ++first)
+		{
+			*first = value;
+		}
+		return first;
+	}
+	template<class Size>
+	char* fill_n(char* first, Size n, const char& value) 
+	{
+		memset(first, static_cast<unsigned char>(value), n);
+		return first + n;
+	}
+	template<class Size>
+	wchar_t* fill_n(wchar_t* first, Size n, const wchar_t& value)
+	{
+		memset(first, static_cast<unsigned char>(value), n * sizeof(wchar_t));
+		return first + n;
+	}
 	
 	
 	
