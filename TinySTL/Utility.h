d95cc65c9b14c8db06bca61fbf2f2ade61711c2a
linxiaoye <512895265@qq.com>
2017-03-23 13:18:30 +0800

    add swap

---
+++ TinySTL/Utility.h
@@ -0,0 +1,24 @@
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+
+/*
+*  Author   @linxiaoye
+*/
+
+namespace TinySTL
+{
+	/************************[ swap ]*********************************/
+	template<class T>
+	void swap(T& lhs, T& rhs)
+	{
+		T tmp(lhs);
+		lhs = rhs;
+		rhs = tmp;
+	}
+	
+	
+	
+}    // namespace TinySTL
+
+#endif  // _UTILITY_H_
+
