3413f0d77fc97bb5b30957778c6b8ca236484ffa
yyehl <512895265@qq.com>
2017-04-02 01:49:28 +0800

    add code

--- TinySTL/Detail/Deque.impl.h
+++ TinySTL/Detail/Deque.impl.h
@@ -140,44 +140,161 @@ namespace TinySTL
 		map_allocator::deallocate(map, map_size);
 		map_size = 0;
 		map = nullptr;
+		start.~dq_iter();
+		finish.~dq_iter();
 	}
 
 	template<class T, class Alloc, size_t Buf_sz>
 	auto deque<T, Alloc, Buf_sz>::operator=(const deque& x) -> deque&
 	{
-
+		if (*this != x) 
+		{
+			~deque();
+			deque(x);
+		}
+		return *this;
 	}
 	template<class T, class Alloc, size_t Buf_sz>
 	auto deque<T, Alloc, Buf_sz>::operator=(deque&& x) -> deque&
 	{
-
+		start = x.start;
+		finish = x.finish;
+		map = x.map;
+		map_size = x.map_size;
+		x.start = x.finish = iterator();
+		map = nullptr;
+		map_size = 0;
+		
 	}
 	
 	/*  其他元素操作函数 */
 	template<class T, class Alloc, size_t Buf_sz>
-	void deque<T, Alloc, Buf_sz>::push_back(const value_type& val)
-	{
-
-	}
-	template<class T, class Alloc, size_t Buf_sz>
 	void deque<T, Alloc, Buf_sz>::push_front(const value_type& val)
 	{
-
+		if (start.cur != start.first) // 如果缓存行头部还有至少还有一个空间
+		{
+			--start.cur;
+			data_allocator::construct(start.cur, val);
+		}
+		else if (start.node != map) // 如果map头部至少还有一个空间
+		{
+			map_pointer tmp = map;
+			*tmp = data_allocator::allocate(buffer_size());
+			start.set_node(tmp);
+			start.cur = start.last;
+			data_allocator::construct(start.cur, val);
+		}
+		else // map已经没有空间了，需要重新安排map
+		{
+			difference_type tmp = finish.node - start.node;
+			if (map_size - tmp >= 2) //如果map尾部还剩2个及以上空间
+			{                                               //那么就原map往后平移
+				
+				uninitialized_copy(start.node, finish.node, map + (map_size - tmp) / 2);
+				start.node = map + tmp / 2;
+				finish.node = start.node + tmp;
+				start.set_node(start - 1);
+				start.cur = start.last;
+				data_allocator::construct(start.cur, val);
+			}
+			else          // 如果尾部空间也不够了，那就必须重新分配map空间
+			{
+				size_type new_map_size = map_size * 2;
+				map_pointer new_map = map_allocator::allocate(new_map_size);
+				uninitialized_copy(start.node, finish.node, new_map + new_map_size / 4);
+				map_allocator::destroy(start.node, finish.node);
+				map_allocator::deallocate(map, map_size);
+				map = new_map;
+				map_size = new_map_size;
+				start.node = new_map + new_map_size / 4;
+				finish.node = start.node + tmp;
+				start.set_node(start - 1);
+				start.cur = start.last;
+				data_allocator::construct(start.cur, val);
+			}
+		}
+	}
+	template<class T, class Alloc, size_t Buf_sz>
+	void deque<T, Alloc, Buf_sz>::push_back(const value_type& val)
+	{
+		if (finish.cur != finish.last - 1)  // 如果这段缓存区尾部还有大于一个空间
+		{
+			data_allocator::construct(finish.cur, val);
+		}
+		else if (finish.node != map + map_size - 1) // 如果map尾部还有至少一个空间，那只需要分配一段缓存区
+		{
+			data_allocator::construct(finish.cur, val);
+			finish.set_node(finish.node + 1);
+			finish.cur = finish.first;
+		}
+		else  // 如果map尾部的空间也不足一个了。。
+		{
+			difference_type tmp = finish.node - start.node;
+			if (map_size - tmp >= 2) // 头部空间还大于两个
+			{
+				uninitialized_copy(start.node, finish.node, map + (map_size - tmp) / 2);
+				start.node = map + (map_size - tmp) / 2;
+				finish.node = start.node + tmp;
+				data_allocator::construct(finish.cur, val);
+				finish.set_node(finish.node + 1);
+				finish.cur = finish.first;
+			} 
+			else   //如果头部也没有两个以上空间了，就必须重新再分配map了
+			{
+				size_type new_map_size = 2 * map_size;
+				map_pointer new_map = map_allocator::allocate(new_map_size);
+				uninitialized_copy(start.node, finish.node, new_map + new_map_size / 4);
+				map_allocator::destroy(start.node, finish.node);
+				map_allocator::deallocate(map, map_size);
+				start.node = new_map + new_map_size / 4;
+				finish.node = start.node + tmp;
+				map = new_map; 
+				map_size = new_map_size;
+				data_allocator::construct(finish.cur, val);
+				finish.set_node(finish.node + 1);
+				finish.cur = finish.first;
+			}
+		}
 	}
 	template<class T, class Alloc, size_t Buf_sz>
 	void deque<T, Alloc, Buf_sz>::pop_back()
 	{
-
+		if (finish.cur != finish.first) // 如果该段缓存区剩余元素大于1
+		{
+			--finish.cur;
+			data_allocator::destroy(finish.cur);
+		}
+		else  // 如果该段缓存区已经没有元素了，则需要回收这段缓存区
+		{
+			data_allocator::deallocate(finish.first, buffer_size());
+			finish.set_node(finish.node - 1);
+			finish.cur = finish.last - 1;
+			data_allocator::destroy(finish.cur);
+		}
 	}
 	template<class T, class Alloc, size_t Buf_sz>
 	void deque<T, Alloc, Buf_sz>::pop_front()
 	{
-
+		if (start.cur != start.last - 1) //如果该缓存区剩余元素大于1
+		{
+			data_allocator::destroy(start.cur);
+			++start.cur;
+		}
+		else  //否则，就需要回收这一段缓存区
+		{
+			data_allocator::destroy(start.cur);
+			data_allocator::deallocate(start.first, buffer_size());
+			start.set_node(start.node + 1);
+			start.cur = start.first;
+		}
 	}
 	template<class T, class Alloc, size_t Buf_sz>
-	void deque<T, Alloc, Buf_sz>::swap()
+	void deque<T, Alloc, Buf_sz>::swap(deque& x)
 	{
-
+		swap(start, x.start);
+		swap(finish, x.finish);
+		swap(map, x.map);
+		swap(map_size, x.map_size);
 	}
 	template<class T, class Alloc, size_t Buf_sz>
 	void deque<T, Alloc, Buf_sz>::clear()
@@ -199,6 +316,17 @@ namespace TinySTL
 			data_allocator::destroy(start.cur, start.last);
 		}
 		finish = start; // 尾迭代器与首迭代器完全一样，指向同一个地方
+	}
+
+	template<class T, class Alloc, size_t Buf_sz>
+	bool deque<T, Alloc, Buf_sz>::operator == (const deque& x) const
+	{
+		return start == x.start && finish == x.finish && map == x.map && map_size = x.map_size;
+	}
+	template<class T, class Alloc, size_t Buf_sz>
+	bool deque<T, Alloc, Buf_sz>::operator != (const deque& x) const
+	{
+		return !(this -> operator == (x));
 	}
 
 	/*  内部工具函数 */
@@ -237,7 +365,7 @@ namespace TinySTL
 	{
 		deque(static_cast<size_type>(first), last);
 	}
-	template<class T, class Alloc, class Buf_sz>
+	template<class T, class Alloc, size_t Buf_sz>
 	template<class InputIterator>
 	void deque<T, Alloc, Buf_sz>::deque_aux(InputIterator first, InputIterator last, std::false_type)
 	{
@@ -251,12 +379,22 @@ namespace TinySTL
 
 
 	/*  外部友元函数 */
-
-
+	template<class T, class Alloc, size_t Buf_sz>
+	bool operator == (const deque<T, Alloc, Buf_sz>& lhs, const deque<T, Alloc, Buf_sz>& rhs)
+	{
+		return lhs.operator==(rhs);
+	}
+	template<class T, class Alloc, size_t Buf_sz>
+	bool operator != (const deque<T, Alloc, Buf_sz>& lhs, const deque<T, Alloc, Buf_sz>& rhs)
+	{
+		return lhs.operator!=(rhs);
+	}
+	template<class T, class Alloc, size_t Buf_sz>
+	void swap(deque<T, Alloc, Buf_sz>& lhs, deque<T, Alloc, Buf_sz>& rhs)
+	{
+		lhs.swap(rhs);
+	}
 
 }  // namespace TinySTL
-
-
-
 
 #endif  // _DEQUE_IMPL_H_
