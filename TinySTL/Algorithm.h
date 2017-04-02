e41be6e29e4cb068c3a900f1c49d1b28694779bb
yyehl <512895265@qq.com>
2017-04-03 02:06:03 +0800

    add heap

--- TinySTL/Algorithm.h
+++ TinySTL/Algorithm.h
@@ -18,7 +18,7 @@ namespace TinySTL
 {
 	
 	/********************[ fill_n ]***********************************/
-	/********************[Algorithm Time Complexity: O(n)]************/
+	/********************[Algorithm Time Complexity: O(N)]************/
 	
 	template<class OutputIterator, class Size, class T>
 	OutputIterator fill_n(OutputIterator first, Size n, const T& value)
@@ -56,7 +56,103 @@ namespace TinySTL
 
 
 
-	
+	/********************[ down_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(logN)]************/
+	template<class RandomIterator>                                              // down_heap是在顶端元素与末尾元素交换了位置之后才能有效执行的
+	void down_heap(RandomIterator first, RandomIterator last, RandomIterator head)  // [first, last]
+	{
+		if (first != last)
+		{
+			size_t index = first - head;
+			size_t right_child_index = 2 * index + 2;
+			while (right_child_index < (last - head))  // 执行下朔操作，必定是在被pop了一个最大值时，即顶部为空
+			{
+				if (*(head + right_child_index) < *(head + right_child_index - 1))
+				{
+					--right_child_index;
+				}
+				if (*(head + index > *(head + right_child_index)))  break;
+				*(head + index) = *(head + right_child_index);
+				index = right_child_index;
+				right_child_index = 2 * index + 2;
+				if (right_child_index == (last - head))  // 如果最底下的只有左叶子，没有右叶子
+				{
+					*(head + index) = *(head + right_child_index - 1);
+				}
+			}
+		}
+	}
+
+	/********************[ up_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(logN)]************/
+	template<class RandomIterator, class Compare>
+	void up_heap(RandomIterator first, RandomIterator last, RandomIterator head)      // [first, last]
+	{                                                                                               
+		if (first != last)
+		{
+			size_t index = last - head;
+			size_t parent_index = (index - 1) / 2;
+			while (index > 0 && *(head + parent_index) < *(head + index))
+			{
+				swap(*(head + parent_index), *(head + index));
+				index = parent_index;
+				parent_index = (index - 1) / 2;
+			}
+		}
+	}
+
+	/********************[ push_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(logN)]************/
+	template<class RandomIterator>                               // push_heap之前，元素是已经添加到了容器末尾的
+	void push_heap(RandomIterator first, RandomIterator last)    // [first, last)
+	{
+		if (first != last)
+		{
+			up_heap(first, last - 1, first);
+		}
+	}
+
+	/********************[ pop_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(logN)]************/
+	template<class RandomIterator>
+	void pop_heap(RandomIterator first, RandomIterator last)   // [first, last)
+	{
+		if (first != last)
+		{
+			swap(*first, *(last - 1);
+			down_heap(first, last - 2, first);
+		}
+
+	}
+	/********************[ make_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(NlogN)]************/
+	template<class RandomIterator>
+	void make_heap(RandomIterator first, RandomIterator last)  // [first, last)
+	{
+		auto index = last - first;
+		if (index < 2) return; // 如果长度为0或1，就不需要操作了
+		auto parent_index = (index - 2) / 2;
+		while (parent_index)
+		{
+			down_heap(first + parent_index, last - 1, first);
+			--parent_index;
+		}
+	}
+
+	/********************[ sort_heap ]***********************************/
+	/********************[Algorithm Time Complexity: O(NlogN)]************/
+	template<class RandomIterator>
+	void sort_heap(RandomIterator first, RandomIterator last)  // [first, last)
+	{
+		if (last - first < 2) return;
+		while (last - first > 1)
+		{
+			pop_heap(first, --last);
+		}
+	}
+
+
+
 }   // namespace TinySTL
 
 #endif  // _ALGORITHM_H_
