e220c916a8be3699487baf4d412363958608bf22
yyehl <512895265@qq.com>
2017-04-04 01:34:53 +0800

    add

--- TinySTL/Test/yyehl_test.cpp
+++ TinySTL/Test/yyehl_test.cpp
@@ -1,11 +1,20 @@
+#include "..\Vector.h"
+#include <iostream>
+#include <stdlib.h>
 
-
-
-
-
-
-int main(int n )
+int main()
 {
-	return 0;
+	TinySTL::vector<int> ivec(10);
+	// std::cout << ivec[0] << " " << ivec[1] << std::endl;
 
+	
+	ivec.push_back(1);
+	ivec.push_back(2);
+	for (auto i : ivec)  std::cout << i << " ";
+	system("pause");
+	std::cout << std::endl;   
+
+	system("pause");
+
+	return 0;
 }
