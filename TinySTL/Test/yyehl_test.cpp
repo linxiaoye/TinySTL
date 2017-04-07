63f206337dd5085dc2334d7336a98b1dff881120
Hualin Ye <yyehl@foxmail.com>
2017-04-07 22:42:52 +0800

    Update yyehl_test.cpp

--- TinySTL/Test/yyehl_test.cpp
+++ TinySTL/Test/yyehl_test.cpp
@@ -4,16 +4,6 @@
 
 int main()
 {
-	TinySTL::vector<int> ivec(10);
-
-	
-	ivec.push_back(1);
-	ivec.push_back(2);
-	for (auto i : ivec)  std::cout << i << " ";
-	system("pause");
-	std::cout << std::endl;   
-
-	system("pause");
 
 	return 0;
 }
