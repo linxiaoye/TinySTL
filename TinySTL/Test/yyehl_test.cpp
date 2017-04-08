2ed917613756d9a423bd58776c8b587a562bbede
yyehl <512895265@qq.com>
2017-04-09 00:33:45 +0800

    fix

--- TinySTL/Test/yyehl_test.cpp
+++ TinySTL/Test/yyehl_test.cpp
@@ -4,6 +4,17 @@
 
 int main()
 {
+	TinySTL::vector<int> ivec(10);
+	// std::cout << ivec[0] << " " << ivec[1] << std::endl;
+
+	
+	ivec.push_back(1);
+	ivec.push_back(2);
+	for (auto i : ivec)  std::cout << i << " ";
+	system("pause");
+	std::cout << std::endl;   
+
+	system("pause");
 
 	return 0;
 }
