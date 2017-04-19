8f0436cd9d52b6dba7d2cc550dff6b8990db92bd
yyehl <512895265@qq.com>
2017-04-19 23:24:13 +0800

    add algorithm code

--- TinySTL/Algorithm.h
+++ TinySTL/Algorithm.h
@@ -8,7 +8,7 @@
 #include <cstring>
 #include <utility>
 #include "Allocator.h"
-// #include "Functional.h"
+#include "Functional.h"
 #include "Iterator.h"
 #include "TypeTraits.h"
 #include "Utility.h"
@@ -16,10 +16,43 @@
 
 namespace TinySTL 
 {
-	
+	/********************[ equal ]***********************************/
+	/********************[Algorithm Time Complexity: O(N)]************/
+	// 如果第二个序列比第一个序列长，那么多余的部分将不予考虑
+	// 如果需要判断两个序列完全一样，那么需要在调用equal函数之前先判断两个序列的长度是否一样长
+	template<class InputIterator1, class InputIterator2>
+	inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		for (; first1 != last1; ++first1, ++first2) 
+		{
+			if (*first1 != *first2)
+				return false;
+			return true;
+		}
+	}
+	template<class InputIterator1, class InputIterator2, class BinaryPredicate>
+	inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate eq)
+	{
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!eq(*first1, *first2))
+				return false;
+			return true;
+		}
+	}
+
+	/********************[ fill ]***********************************/
+	/********************[Algorithm Time Complexity: O(N)]************/
+	template<class ForwardIterator, class T>
+	void fill(ForwardIterator first, ForwardIterator last, const T& val)
+	{
+		for (; first != last; ++first)
+			*first = val;
+	}
+
 	/********************[ fill_n ]***********************************/
 	/********************[Algorithm Time Complexity: O(N)]************/
-	
+	// 如果n大于了序列的长度，那么将会造成不可预期的结果
 	template<class OutputIterator, class Size, class T>
 	OutputIterator fill_n(OutputIterator first, Size n, const T& value)
 	{
@@ -41,7 +74,73 @@ namespace TinySTL
 		memset(first, static_cast<unsigned char>(value), n * sizeof(wchar_t));
 		return first + n;
 	}
-	
+
+	/********************[ iter_swap ]***********************************/
+	/********************[Algorithm Time Complexity: O(1)]************/
+	template<class ForwardIterator1, class ForwardIterator2>
+	void iter_swap(ForwardIterator1 iter1, ForwardIterator2 iter2)
+	{
+		iterator_traits<ForwardIterator1>::value_type tmp = *iter1;
+		*iter1 = *iter2;
+		*iter2 = tmp;
+	}
+
+	/********************[ lexicographical_compare ]***********************************/
+	/********************[Algorithm Time Complexity: O(n)]************/
+	// 字典排列方式排序
+	template<class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		for (; first1 != last1 && first2 != last2; ++first1, ++first2)
+		{
+			if (*first1 < *first2)
+				return true;
+			if (*first2 < *first1)
+				return false;
+			// 还继续进行就说明当前两序列相等
+		}
+		return first1 == last1 && first2 != last2;
+	}
+	template<class InputIterator1, class InputIterator2, class BinaryCompare>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryCompare cmp)
+	{
+		for (; first1 != last1 && first2 != last2; ++first1, ++first2)
+		{
+			if (cmp(*first1, *first2))
+				return true;
+			if (cmp(*first2, *first2))
+				return false;
+			// 还继续进行就说明当前两序列相等
+		}
+		return first1 == last1 && first2 != last2;
+	}
+
+	/********************[ max ]***********************************/
+	/********************[Algorithm Time Complexity: O(1)]************/
+	template<class T>
+	inline const T& max(const T& x, const T& y)
+	{
+		return x < y ? y : x;
+	}
+	template<class T, class Compare>
+	inline const T& max(const T& x, const T& y, Compare cmp)
+	{
+		return cmp(x, y) ? y : x;
+	}
+
+	/********************[ min ]***********************************/
+	/********************[Algorithm Time Complexity: O(1)]************/
+	template<class T>
+	inline const T& min(const T& x, const T& y)
+	{
+		return x < y ? x : y;
+	}
+	template<class T, class Compare>
+	inline const T& min(const T& x, const T& y, Compare cmp)
+	{
+		return cmp(x, y) ? x : y;
+	}
+
 	/********************[ less ]***********************************/
 	/********************[Algorithm Time Complexity: O(1)]************/	
 	template<class T>
@@ -50,12 +149,36 @@ namespace TinySTL
 		return lhs < rhs;
 	}
 
+	/********************[ mismatch ]***********************************/
+	/********************[Algorithm Time Complexity: O(N)]************/
+	// 返回两个序列首次出现不同的地方，返回一个pair
+	template<class InputIterator1, class InputIterator2>
+	pair<InputIterator1, InputIterator2> mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1 != last1 && *first1 == *first2)
+		{
+			++first1;
+			++first2;
+		}
+		return pair<InputIterator1, InputIterator2>(first1, first2);
+	}
+	template<class InputIterator1, class InputIterator2, class BinaryEqual>
+	pair<InputIterator1, InputIterator2> mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryEqual eq)
+	{
+		while (first1 != last1 && eq(*first1, *first2))
+		{
+			++first1;
+			++first2;
+		}
+		return pair<InputIterator1, InputIterator2>(first1, first2);
+	}
+
 	/********************[ copy ]***********************************/
 	/********************[Algorithm Time Complexity: O(n)]************/
 	
 
 
-
+	/****************************************************************************************[heap相关]****************************************/
 	/********************[ down_heap ]***********************************/
 	/********************[Algorithm Time Complexity: O(logN)]************/
 	template<class RandomIterator>                                              // down_heap是在顶端元素与末尾元素交换了位置之后才能有效执行的
@@ -150,9 +273,24 @@ namespace TinySTL
 			pop_heap(first, --last);
 		}
 	}
+	/****************************************************************************************[heap相关]*************************************/
 
+
+
+
+
+
+
+
+
+
+	
 
 
 }   // namespace TinySTL
+
+/****************************************[数值类算法]***********************************************/
+
+#include "Detail\Numeric.h"
 
 #endif  // _ALGORITHM_H_
