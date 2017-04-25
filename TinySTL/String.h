5a489afb74e392d5233e08de625c3cd7f4be8243
yyehl <512895265@qq.com>
2017-04-25 21:26:17 +0800

    add string.h

---
+++ TinySTL/String.h
@@ -0,0 +1,35 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+/*
+*	Author  @yyehl
+*/
+
+namespace TinySTL
+{
+	class string
+	{
+		typedef char          value_type;
+		typedef char*         iterator;
+		typedef const char*   const_itereator;
+		typedef char&         reference;
+		typedef const char&   const_reference;
+		typedef size_t        size_type;
+		typedef ptrdiff_t     difference_type;
+
+
+
+
+
+
+	};
+
+
+
+
+}  // namespace TinySTL
+
+
+
+
+#endif // !_STRING_H_
