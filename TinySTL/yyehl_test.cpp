f47d4b0ff2e152824ab84ddcddd388b33d2a6113
yyehl <512895265@qq.com>
2017-05-02 21:18:11 +0800

    add code

--- TinySTL/yyehl_test.cpp
+++ TinySTL/yyehl_test.cpp
@@ -3,13 +3,14 @@
 #include "Test\Vector_Test.h"
 #include "Test\Algorithm_Test.h"
 
+#include <vector>
+#include <iostream>
 
 int main()
 {
 	TinySTL::VectorTest::testAllCases();
 	TinySTL::testAll_algorithm_Cases();
-
-
+	
 
 
 	system("pause");
