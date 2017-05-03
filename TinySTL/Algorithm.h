3a5967310c5fdcd08b2b404ebc39921997c93d60
yyehl <512895265@qq.com>
2017-05-03 22:03:16 +0800

    fix bugs

--- TinySTL/Algorithm.h
+++ TinySTL/Algorithm.h
@@ -201,104 +201,114 @@ namespace TinySTL
 
 
 
-	/****************************************************************************************[heap相关]****************************************/
-	/********************[ down_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(logN)]************/
-	template<class RandomIterator>                                              // down_heap是在顶端元素与末尾元素交换了位置之后才能有效执行的
-	void down_heap(RandomIterator first, RandomIterator last, RandomIterator head)  // [first, last]
-	{
-		if (first != last)
-		{
-			size_t index = first - head;
-			size_t right_child_index = 2 * index + 2;
-			while (right_child_index < (size_t)(last - head))  // 执行下朔操作，必定是在被pop了一个最大值时，即顶部为空
-			{
-				if (*(head + right_child_index) < *(head + right_child_index - 1))
-				{
-					--right_child_index;
-				}
-				if (*(head + right_child_index) < *(head + index))  break;
-				*(head + index) = *(head + right_child_index);
-				index = right_child_index;
-				right_child_index = 2 * index + 2;
-				if (right_child_index == (last - head))  // 如果最底下的只有左叶子，没有右叶子
-				{
-					*(head + index) = *(head + right_child_index - 1);
-				}
+
+	//********** [make_heap] ***************
+	//********* [Algorithm Complexity: O(N)] ****************
+	template<class RandomAccessIterator, class Compare>
+	//heap上溯算法
+	static void up(RandomAccessIterator first, RandomAccessIterator last,
+		RandomAccessIterator head, Compare comp) {//1.[first, last], 2.headr points the header of the heap
+		if (first != last) {
+			int index = last - head;
+			auto parentIndex = (index - 1) / 2;
+			for (auto cur = last; parentIndex >= 0 && cur != head; parentIndex = (index - 1) / 2) {
+				auto parent = head + parentIndex;//get parent
+				if (comp(*parent, *cur))
+					TinySTL::swap(*parent, *cur);
+				cur = parent;
+				index = cur - head;
 			}
 		}
 	}
-
-	/********************[ up_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(logN)]************/
-	template<class RandomIterator>
-	void up_heap(RandomIterator first, RandomIterator last, RandomIterator head)      // [first, last]
-	{                                                                                               
-		if (first != last)
-		{
-			size_t index = last - head;
-			size_t parent_index = (index - 1) / 2;
-			while (index > 0 && *(head + parent_index) < *(head + index))
-			{
-				swap(*(head + parent_index), *(head + index));
-				index = parent_index;
-				parent_index = (index - 1) / 2;
+	template<class RandomAccessIterator, class Compare>
+	//heap下降算法
+	static void down(RandomAccessIterator first, RandomAccessIterator last,
+		RandomAccessIterator head, Compare comp) {//1.[first, last], 2.headr points the header of the heap
+		if (first != last) {
+			auto index = first - head;
+			auto leftChildIndex = index * 2 + 1;
+			for (auto cur = first; leftChildIndex < (last - head + 1) && cur < last; leftChildIndex = index * 2 + 1) {
+				auto child = head + leftChildIndex;//get the left child
+				if ((child + 1) <= last && *(child + 1) > *child)//cur has a right child
+					child = child + 1;
+				if (comp(*cur, *child))
+					TinySTL::swap(*cur, *child);
+				cur = child;
+				index = cur - head;
 			}
 		}
 	}
-
-	/********************[ push_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(logN)]************/
-	template<class RandomIterator>                               // push_heap之前，元素是已经添加到了容器末尾的
-	void push_heap(RandomIterator first, RandomIterator last)    // [first, last)
-	{
-		if (first != last)
-		{
-			up_heap(first, last - 1, first);
+	template <class RandomAccessIterator>
+	void make_heap(RandomAccessIterator first, RandomAccessIterator last) {
+		TinySTL::make_heap(first, last,
+			typename TinySTL::less<TinySTL::iterator_traits<RandomAccessIterator>::value_type>());
+	}
+	template <class RandomAccessIterator, class Compare>
+	void make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp) {
+		const auto range = last - first;
+		for (auto cur = first + range / 2 - 1; cur >= first; --cur) {
+			TinySTL::down(cur, last - 1, first, comp);
+			if (cur == first) return;
 		}
 	}
-
-	/********************[ pop_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(logN)]************/
-	template<class RandomIterator>
-	void pop_heap(RandomIterator first, RandomIterator last)   // [first, last)
-	{
-		if (first != last)
-		{
-			swap(*first, *(last - 1));
-			down_heap(first, last - 2, first);
-		}
-
+	//********* [push_heap] ***************
+	//********* [Algorithm Complexity: O(lgN)] ****************
+	template <class RandomAccessIterator>
+	void push_heap(RandomAccessIterator first, RandomAccessIterator last) {
+		TinySTL::push_heap(first, last,
+			TinySTL::less<typename TinySTL::iterator_traits<RandomAccessIterator>::value_type>());
 	}
-	/********************[ make_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(NlogN)]************/
-	template<class RandomIterator>
-	void make_heap(RandomIterator first, RandomIterator last)  // [first, last)
-	{
-		size_t index = last - first - 1;
-		if (index < 2) return; // 如果长度为0或1，就不需要操作了
-		size_t parent_index = (index - 1) / 2;
-		while (parent_index > 0)
-		{
-			down_heap(first + parent_index, last - 1, first);
-			--parent_index;
-		}
+	template <class RandomAccessIterator, class Compare>
+	void push_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp) {
+		TinySTL::up(first, last - 1, first, comp);
 	}
-
-	/********************[ sort_heap ]***********************************/
-	/********************[Algorithm Time Complexity: O(NlogN)]************/
-	template<class RandomIterator>
-	void sort_heap(RandomIterator first, RandomIterator last)  // [first, last)
-	{
-		if (last - first < 2) return;
-		while (last - first > 1)
-		{
-			--last;
-			TinySTL::pop_heap(first, last);
+	//********* [pop_heap] ***************
+	//********* [Algorithm Complexity: O(lgN)] ****************
+	template <class RandomAccessIterator>
+	void pop_heap(RandomAccessIterator first, RandomAccessIterator last) {
+		TinySTL::pop_heap(first, last,
+			TinySTL::less<typename TinySTL::iterator_traits<RandomAccessIterator>::value_type>());
+	}
+	template <class RandomAccessIterator, class Compare>
+	void pop_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp) {
+		TinySTL::swap(*first, *(last - 1));
+		if (last - first >= 2)
+			TinySTL::down(first, last - 2, first, comp);
+	}
+	//********* [sort_heap] ***************
+	//********* [Algorithm Complexity: O(N)] ****************
+	template <class RandomAccessIterator>
+	void sort_heap(RandomAccessIterator first, RandomAccessIterator last) {
+		return TinySTL::sort_heap(first, last,
+			TinySTL::less<typename TinySTL::iterator_traits<RandomAccessIterator>::value_type>());
+	}
+	template <class RandomAccessIterator, class Compare>
+	void sort_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp) {
+		for (auto cur = last; cur != first; --cur) {
+			TinySTL::pop_heap(first, cur, comp);
 		}
 	}
-	/****************************************************************************************[heap相关]*************************************/
-
+	//********* [is_heap] ***************
+	//********* [Algorithm Complexity: O(N)] ****************
+	template <class RandomAccessIterator>
+	bool is_heap(RandomAccessIterator first, RandomAccessIterator last) {
+		return TinySTL::is_heap(first, last,
+			TinySTL::less<typename TinySTL::iterator_traits<RandomAccessIterator>::value_type>());
+	}
+	template <class RandomAccessIterator, class Compare>
+	bool is_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp) {
+		const auto range = last - first;
+		auto index = range / 2 - 1;
+		for (auto cur = first + range / 2 - 1; cur >= first; --cur, --index) {
+			if (*(first + (index * 2 + 1)) > *cur ||//left child > cur
+				((first + (index * 2 + 2)) <= last && *(first + (index * 2 + 2)) > *cur))//right child > cur
+				return false;
+			if (cur == first)
+				break;
+		}
+		return true;
+	}
+	
 
 	/****************************************************************************************[set相关]*************************************/
 	/********************[ set_union ]***********************************/
